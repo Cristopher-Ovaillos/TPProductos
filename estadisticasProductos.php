846595db58ae5f9c59e5a22f5272b83f17f80827
Cristopher-Ovaillos <cristopher.ovaillos@est.fi.uncoma.edu.ar>
2024-02-19 05:24:03 -0300

    uso del uasort

---
+++ estadisticasProductos.php
@@ -0,0 +1,305 @@
+<?php
+
+function carga_ventas($prodMasVendido)
+{
+    $ventas = [];
+    foreach ($prodMasVendido as $prod) {
+        $precio=$prod["precioProd"];
+        $cant=$prod["cantProd"];
+        $ventas[]=monto_Venta($precio,$cant);  
+    }
+    return $ventas;
+}
+
+function prodMasVendido()
+{
+    $prodMasVendido[0] = ["prod" => 'Heladera Gama', "precioProd" => 90, "cantProd" => 9];
+    $prodMasVendido[1] = ["prod" => 'Microondas Samsung', "precioProd" => 75, "cantProd" => 12];
+    $prodMasVendido[2] = ["prod" => 'TV Smart Samsung 32', "precioProd" => 150, "cantProd" => 8];
+    $prodMasVendido[3] = ["prod" => 'Lavadora LG', "precioProd" => 200, "cantProd" => 5];
+    $prodMasVendido[4] = ["prod" => 'Cafetera Philips', "precioProd" => 40, "cantProd" => 15];
+    $prodMasVendido[5] = ["prod" => 'Aspiradora Dyson', "precioProd" => 180, "cantProd" => 7];
+    $prodMasVendido[6] = ["prod" => 'Aire Acondicionado Panasonic', "precioProd" => 250, "cantProd" => 4];
+    $prodMasVendido[7] = ["prod" => 'Licuadora Oster', "precioProd" => 50, "cantProd" => 10];
+    $prodMasVendido[8] = ["prod" => 'Horno Black+Decker', "precioProd" => 120, "cantProd" => 6];
+    $prodMasVendido[9] = ["prod" => 'Ventilador Honeywell', "precioProd" => 30, "cantProd" => 20];
+    $prodMasVendido[10] = ["prod" => 'Secadora Whirlpool', "precioProd" => 180, "cantProd" => 5];
+    $prodMasVendido[11] = ["prod" => 'Nevera Bosch', "precioProd" => 300, "cantProd" => 3];
+
+    return $prodMasVendido;
+}
+
+function monto_Venta($precioP, $cantP)
+{
+    return $precioP * $cantP;
+}
+//Opcion Nº1
+function opcion_N1(&$ventas, &$prodMasVendido)
+{
+
+    $mes = solicitarMes();
+    $nombre = solicitar_Prod();
+    $precio = solicitar_PrecioU();
+    $cantidad = solicitar_Cantidad();
+    $total = monto_Venta($precio, $cantidad);
+    $ventas[$mes]+= $total;
+    $valorComparar = monto_Venta($prodMasVendido[$mes]["precioProd"], $prodMasVendido[$mes]["cantProd"]);
+
+    // actualizar prodMasVendido
+    if ($total > $valorComparar) {
+        $prodMasVendido[$mes] = [
+            "prod" => $nombre,
+            "precioProd" => $precio,
+            "cantProd" => $cantidad
+        ];
+        echo "Venta ingresada exitosamente y actualizada como producto mas vendido del mes.\n";
+    } else {
+        echo "Venta ingresada exitosamente.\n";
+    }
+}
+
+
+
+function solicitar_Prod()
+{
+    echo "  Ingrese el producto: ";
+    $nombre = trim(fgets((STDIN)));
+    return $nombre;
+}
+
+function solicitar_PrecioU()
+{
+    echo "  Ingrese el VALOR del producto: ";
+    $valor = trim(fgets((STDIN)));
+    return $valor;
+}
+
+function solicitar_Cantidad()
+{
+    echo "  Ingrese la cantidad de producto vendido: ";
+    $cant = trim(fgets((STDIN)));
+    return $cant;
+}
+
+function indice_a_mes($index){
+    $mes = "invalido";
+    $meses[0]="Enero";
+    $meses[1]="Febrero";
+    $meses[2]="Marzo";
+    $meses[3]="Abril";
+    $meses[4]="Mayo";
+    $meses[5]="Junio";
+    $meses[6]="Julio";
+    $meses[7]="Agosto";
+    $meses[8]="Septiembre";
+    $meses[9]="Octubre";
+    $meses[10]="Noviembre";
+    $meses[11]="Diciembre";
+    if ($index >= 0 && $index <= 11) { 
+        $mes = $meses[$index];
+    } 
+    return $mes;
+}
+//Opcion Nº2
+function opcion_N2($ventas)
+{
+    $tamanio = count($ventas);
+    $monto = 0;
+    $mes = 0;
+    for ($i = 0; $i < $tamanio; ++$i) {
+
+        $var = $ventas[$i];
+
+        if ($monto < $var) {
+            $mes = $i;
+            $monto = $var;
+        }
+    }
+
+    $str=indice_a_mes($mes);
+
+    echo "  El mes con mayor ventas es: ", $str, "\n";
+}
+
+
+//Opcion Nº3
+function opcion_N3($ventas)
+{
+    $montoObjetivo = solicitar_MontoObjetivo();
+    $exito = true;
+    $tamanio = count($ventas);
+    $it = 0;
+    while ($exito && $it < $tamanio) {
+
+        if ($ventas[$it] > $montoObjetivo) {
+            $exito = false;
+            echo "Informacion completa del mes:\n";
+            echo "Mes:", indice_a_mes($it), " \n";
+            echo "Monto total de ventas: ", $ventas[$it], "\n";
+            
+        }
+        $it++;
+    }
+}
+
+function solicitar_MontoObjetivo()
+{
+    echo "Ingrese el monto total de ventas objetivo: ";
+    $monto = floatval(trim(fgets(STDIN)));
+    return $monto;
+}
+
+//Opcion Nº4
+function opcion_N4($ventas, $prodMasVendido){
+
+    $mes= solicitarMes();
+    
+    $str= indice_a_mes($mes);
+    echo "<",$str,"> \n";
+    
+    $cant=$prodMasVendido[$mes]["cantProd"];
+    $precio=$prodMasVendido[$mes]["precioProd"];
+    $total=monto_Venta($cant,$precio);
+    echo "El producto con mayor monto de venta:", $prodMasVendido[$mes]["prod"],"\n";
+    echo "Cantidad de Productos Vendidos:", $cant,"\n";
+    echo "Precio Unitario: $", $precio,"\n";
+    echo "Monto de venta del producto: $",  $total,"\n";
+    echo "Monto acumulado de ventas del mes ",  $str,": $",$ventas[$mes],"\n";
+}
+
+function prod_Mas_Ventas($prodMasVendido){
+    $tamanio = count($prodMasVendido);
+    $monto = 0;
+    $mes = 0;
+    for ($i = 0; $i < $tamanio; ++$i) {
+
+        $cant=$prodMasVendido[$i]["cantProd"];
+    $precio=$prodMasVendido[$i]["precioProd"];
+
+        $var = monto_Venta($precio,$cant);
+
+        if ($monto < $var) {
+            $mes = $i;
+            $monto = $var;
+        }
+    }
+    return $mes;
+}
+
+//Opcion Nº5
+//PRINT_R permite una visualizacion de una estructura de datos
+//Propociona una salida legible y sea bastante comprensible.
+//Muestra el indice y sus valores asociados a cada indice
+
+function opcion_N5($prodMasVendido){
+    echo"PRODUCTO ORDENADO \n";
+    $prodMasVendidoOrdenado = ordenarPorBurbuja($prodMasVendido);
+    print_r($prodMasVendidoOrdenado);
+}
+function ordenarPorBurbuja($arr) {
+    $n = count($arr);
+    $arrOrdenado = $arr; 
+
+    for ($i = 0; $i < $n - 1; $i++) {
+        for ($j = 0; $j < $n - $i - 1; $j++) {
+            $cant_1 = $arrOrdenado[$j]["cantProd"];
+            $precio_1 = $arrOrdenado[$j]["precioProd"];
+            $total_1 = monto_Venta($cant_1, $precio_1);
+
+            $cant_2 = $arrOrdenado[$j + 1]["cantProd"];
+            $precio_2 = $arrOrdenado[$j + 1]["precioProd"];
+            $total_2 = monto_Venta($cant_2, $precio_2);
+
+            if ($total_1 < $total_2) {
+                // Intercambio
+                $temp = $arrOrdenado[$j];
+                $arrOrdenado[$j] = $arrOrdenado[$j + 1];
+                $arrOrdenado[$j + 1] = $temp;
+            }
+        }
+    }
+
+    return $arrOrdenado;
+}
+
+//Menu
+function indice_opcion(&$indice, &$ventas, &$prodMasVendido)
+{
+    switch ($indice) {
+        case 1:
+            opcion_N1($ventas, $prodMasVendido);
+            break;
+        case 2:
+            opcion_N2($ventas);
+            break;
+        case 3:
+            opcion_N3($ventas);
+            break;
+        case 4:
+            opcion_N4($ventas,$prodMasVendido);
+            break;
+        case 5:
+            opcion_N5($prodMasVendido);
+            break;
+        case 6:
+                echo "SALIENDO DEL MENU\n";
+            break;  
+    }
+}
+
+function menu_opciones(&$ventas, &$prodMasVendido)
+{
+
+   
+    do {
+        print_r($prodMasVendido);
+        print_r($ventas);
+
+        echo "1) ingresar una venta\n";
+        echo "2) Mes con mayor monto de ventas \n";
+        echo "3) Primer mes que supera un  monto de ventas\n";
+        echo "4) Informacion de un mes \n";
+        echo "5) Producto mas vendido Ordenado \n";
+        echo "6) FIN PROGRAMA \n";
+        echo "ingrese una opcion:   ";
+        $opcion = trim(fgets(STDIN)); //TRIM PARA ELIMINAR ESPACIOS 
+
+        if ($opcion < 1 || $opcion > 6) {
+            echo "OPCION INVALIDA! \n";
+        } else {
+            indice_opcion($opcion, $ventas, $prodMasVendido);
+        }
+    } while ($opcion != 6);
+}
+
+function solicitarMes() {
+    $meses = [
+        'Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio',
+        'Julio', 'Agosto', 'Septiembre', 'Octubre', 'Noviembre', 'Diciembre'
+    ];
+    // Solicitar al usuario que ingrese un mes
+    do {
+        $input = readline("Ingrese el nombre del mes: ");
+        $input = ucfirst(strtolower($input)); 
+    } while (!in_array($input, $meses));
+
+    // Obtener el num de la posicion de ese mes en el
+    $indice = array_search($input, $meses) ; 
+
+    return $indice;
+}
+
+function main()
+{
+    
+
+    $prodMasVendido = prodMasVendido();
+    $ventas = carga_ventas($prodMasVendido);
+
+
+    menu_opciones($ventas, $prodMasVendido);
+    print_r($prodMasVendido);
+    print_r($ventas);
+}
+
+main();
